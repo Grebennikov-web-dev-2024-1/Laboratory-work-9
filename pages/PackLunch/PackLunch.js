bb2d7fa1b9209abe7327b2cbb3fc880cfad1e03a
malivartti <malivartti@yandex.ru>
2024-10-07 15:05:22 +0300

    add laboratory work 3

---
+++ pages/PackLunch/PackLunch.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('.form')
+form.addEventListener('change', (e) => {
+  const deliveryTimeInput = document.getElementById('delivery_time')
+
+  if (e.target.getAttribute('id') == 'by_specified_time') {
+    deliveryTimeInput.required = true
+  } else {
+    deliveryTimeInput.required = false
+  }
+})
