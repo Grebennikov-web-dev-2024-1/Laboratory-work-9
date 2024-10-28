5fc90483278b7dead6cb2ece6c900cae54012384
malivartti <malivartti@yandex.ru>
2024-10-28 21:48:35 +0300

    add laboratory work 4

---
+++ pages/PackLunch/menuList.js
@@ -0,0 +1,74 @@
+export const menu = [
+  {
+    "keyword": "gazpacho",
+    "name": "Гаспачо",
+    "price": 195,
+    "category": "soups",
+    "count": "350 г",
+    "image": "../../img/menu/soups/gazpacho.jpg"
+  },
+  {
+    "keyword": "mushroom_soup",
+    "name": "Грибной суп-пюре",
+    "price": 185,
+    "category": "soups",
+    "count": "330 г",
+    "image": "../../img/menu/soups/mushroom_soup.jpg"
+  },
+  {
+    "keyword": "norwegian_soup",
+    "name": "Норвежский суп",
+    "price": 270,
+    "category": "soups",
+    "count": "330 г",
+    "image": "../../img/menu/soups/norwegian_soup.jpg"
+  },
+  {
+    "keyword": "fried_potatoes_with_mushrooms",
+    "name": "Жареная картошка с грибами",
+    "price": 150,
+    "category": "main-course",
+    "count": "250 г",
+    "image": "../../img/menu/main_course/friedpotatoeswithmushrooms1.jpg"
+  },
+  {
+    "keyword": "lasagna",
+    "name": "Лазанья",
+    "price": 385,
+    "category": "main-course",
+    "count": "310 г",
+    "image": "../../img/menu/main_course/lasagna.jpg"
+  },
+  {
+    "keyword": "chicken_cutlets_and_mashed_potatoes",
+    "name": "Котлеты из курицы с картофельным пюре",
+    "price": 225,
+    "category": "main-course",
+    "count": "280 г",
+    "image": "../../img/menu/main_course/chickencutletsandmashedpotatoes.jpg"
+  },
+  {
+    "keyword": "orange_juice",
+    "name": "Апельсиновый сок",
+    "price": 120,
+    "category": "beverages",
+    "count": "300 мл",
+    "image": "../../img/menu/beverages/orangejuice.jpg"
+  },
+  {
+    "keyword": "apple_juice",
+    "name": "Яблочный сок",
+    "price": 90,
+    "category": "beverages",
+    "count": "300 мл",
+    "image": "../../img/menu/beverages/applejuice.jpg"
+  },
+  {
+    keyword: 'carrot_juice',
+    name: 'Морковный сок',
+    price: 110,
+    category: 'beverages',
+    count: '300 мл',
+    image: '../../img/menu/beverages/carrotjuice.jpg',
+  },
+]
