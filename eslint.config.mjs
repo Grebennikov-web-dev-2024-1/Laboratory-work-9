0c21a7f9ff7f9f3b5eae9ae9dd642af5bbd74ef2
malivartti <malivartti@yandex.ru>
2024-11-04 22:24:29 +0300

    add laboratory work 5

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,30 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
-/** @type {import('eslint').Linter.Config[]}*/
-export default [
-    {languageOptions: { globals: globals.browser }},
-    pluginJs.configs.recommended,
-    {
-        ignores: ['node_modules']
+export default [{
+    languageOptions: {
+        globals: {},
+        ecmaVersion: "latest",
+        // sourceType: "script",
     },
-    {
-        rules: {
-            indent: "error",
-            "space-before-blocks": "error",
-            "keyword-spacing": "error",
-            "space-infix-ops": "error",
-            "space-in-parens": "error",
-            "brace-style": "error",
-            "comma-spacing": "error",
-            "array-bracket-spacing": "error",
-            "no-multi-spaces": "error",
-            "no-multiple-empty-lines": "error",
-            "computed-property-spacing": "error",
-            "func-call-spacing": "error",
 
-            "max-len": ["error", {
-                code: 80,
-            }],
+    rules: {
+        indent: "error",
+        "space-before-blocks": "error",
+        "keyword-spacing": "error",
+        "space-infix-ops": "error",
+        "space-in-parens": "error",
+        "brace-style": "error",
+        "comma-spacing": "error",
+        "array-bracket-spacing": "error",
+        "no-multi-spaces": "error",
+        "no-multiple-empty-lines": "error",
+        "computed-property-spacing": "error",
+        "func-call-spacing": "error",
 
-            semi: "error",
-            "no-use-before-define": "error",
-            "no-array-constructor": "error",
-        }
-    }
-];
+        "max-len": ["error", {
+            code: 80,
+        }],
+
+        semi: "error",
+        "no-use-before-define": "error",
+        "no-array-constructor": "error",
+    },
+}];
--- pages/PackLunch/mountMenu.js
+++ pages/PackLunch/mountMenu.js
@@ -1,158 +1,173 @@
 import { menu } from './menuList.js';
 
 const selectedMenuItems = {
-    soups: null,
+    "soups": null,
     "main-course": null,
-    beverages: null,
+    "salads-starters": null,
+    "beverages": null,
+    "desserts": null,
 };
- 
-const dishCardTemplate = document.getElementById('dish-card');
 
-const soupsListNode = document.querySelector('.soups-section div.dishes');
-const mainCourseListNode = (
-    document.querySelector('.main-course-section div.dishes')
-);
-const beveragesListNode = (
-    document.querySelector('.beverages-section div.dishes')
-);
+const textBasketMenuItem = {
+    "soups": {
+        title: "Суп",
+        default: "Суп не выбран"
+    },
+    "main-course": {
+        title: "Главное блюдо",
+        default: "Главное блюдо не выбрано"
+    },
+    "salads-starters": {
+        title: "Салат/стартер",
+        default: "Салат/стартер не выбран"
+    },
+    "beverages": {
+        title: "Напиток",
+        default: "Напиток не выбран"
+    },
+    "desserts": {
+        title: "Десерт",
+        default: "Десерт не выбран"
+    },
+};
 
-menu
-    .sort((a, b) => a.name.localeCompare(b.name))
-    .forEach((menuItem) => {
-        const menuItemNode = dishCardTemplate.content.cloneNode(true);
-        const rootNode = menuItemNode.querySelector('.dish');
-        rootNode.dataset.dish = menuItem.keyword;
-        const img = menuItemNode.querySelector('.dish__img');
-        img.src = menuItem.image;
-        img.alt = menuItem.name;
-        menuItemNode.querySelector('.dish__price').textContent = menuItem.price;
-        menuItemNode.querySelector('.dish__name').textContent = menuItem.name;
-        menuItemNode.querySelector('.dish__amount').textContent = (
-            menuItem.count
-        );
+const createBasketMenuItem = (title, content) => {
+    return `
+    <div class="basket-menu-item">
+        <p class="basket-menu-item__title">${title}</p>
+        <p class="basket-menu-item__content">${content}</p>
+    </div>
+    `;
+};
 
-        menuItemNode
-            .querySelector('.dish__add')
-            .addEventListener('click', (e) => {
-                const keywordOfSelectedMenuItem = (
-                    e.target.closest('.dish').dataset.dish
-                );
-                const selectedMenuItem = (
-                    menu.find((item) => (
-                        item.keyword == keywordOfSelectedMenuItem)
-                    )
-                );
-                selectedMenuItems[selectedMenuItem.category] = selectedMenuItem;
 
-                const menuItemsByCategory = (
-                    document
-                        .querySelectorAll(
-                            `.${selectedMenuItem.category}-section .dish`
-                        )
-                );
-                menuItemsByCategory.forEach((itemNode) => {
-                    if (itemNode.dataset.dish === keywordOfSelectedMenuItem) {
-                        itemNode.classList.add('dish_selected');
-                    } else {
-                        itemNode.classList.remove('dish_selected');
-                    }
-                });
-    
-                const formBasketMenuItems = (
-                    document.querySelector('.form__basket-menu-items')
-                );
-                const basketMenuItems = (
-                    document
-                        .getElementById('basket-menu-items')
-                        .content
-                        .cloneNode(true)
-                );
-
-                const basketMenuItemSoups = (
-                    basketMenuItems.getElementById('basket-menu-item-soups')
-                );
-                const basketMenuItemSoupsValue = (
-                    selectedMenuItems.soups 
-                        ? (
-                            `${selectedMenuItems.soups.name} `
-                            + `${selectedMenuItems.soups.price}₽`
-                        )
-                        : 'Суп не выбран'
-                );
-                basketMenuItemSoups
-                    .querySelector('.basket-menu-item__content')
-                    .textContent = basketMenuItemSoupsValue;
-                document.getElementById('soups').value = (
-                    selectedMenuItems.soups 
-                        ? selectedMenuItems.soups.keyword 
-                        : ""
-                );
-
-                const basketMenuItemMainCourse = (
-                    basketMenuItems
-                        .getElementById('basket-menu-item-main-course')
-                );
-                const basketMenuItemMainCourseValue = (
-                    selectedMenuItems['main-course'] 
-                        ? (
-                            `${selectedMenuItems['main-course'].name} `
-                            + `${selectedMenuItems['main-course'].price}₽` 
-                        )
-                        : 'Главное блюдо не выбрано'
-                );
-                basketMenuItemMainCourse
-                    .querySelector('.basket-menu-item__content')
-                    .textContent = basketMenuItemMainCourseValue;
-                document.getElementById('main-course').value = (
-                    selectedMenuItems['main-course'] 
-                        ? selectedMenuItems['main-course'].keyword 
-                        : ""
-                );
-
-                const basketMenuItemBeverages = (
-                    basketMenuItems
-                        .getElementById('basket-menu-item-beverages')
-                );
-                const basketMenuItemBeveragesValue = (
-                    selectedMenuItems.beverages 
-                        ? (
-                            `${selectedMenuItems.beverages.name} ` 
-                            + `${selectedMenuItems.beverages.price}₽`
-                        )
-                        : 'Напиток не выбран'
-                );
-                basketMenuItemBeverages
-                    .querySelector('.basket-menu-item__content')
-                    .textContent = basketMenuItemBeveragesValue;
-                document.getElementById('beverages').value = (
-                    selectedMenuItems.beverages 
-                        ? selectedMenuItems.beverages.keyword 
-                        : ""
-
-                );
-
-                const basketMenuItemSum = (
-                    basketMenuItems
-                        .getElementById('basket-menu-item-sum')
-                );
-                const basketMenuItemSumValue = (
-                    Object
-                        .values(selectedMenuItems)
-                        .reduce((acc, cur) => cur ? cur.price + acc : acc, 0)
-                );
-                basketMenuItemSum
-                    .querySelector('.basket-menu-item__content')
-                    .textContent = `${basketMenuItemSumValue}₽`;
-
-                formBasketMenuItems.innerHTML = '';
-                formBasketMenuItems.append(basketMenuItems);
-            });
-
-        if (menuItem.category == 'soups') {
-            soupsListNode.append(menuItemNode);
-        } else if (menuItem.category == 'main-course') {
-            mainCourseListNode.append(menuItemNode);
-        } else if (menuItem.category == 'beverages') {
-            beveragesListNode.append(menuItemNode);
+const changeDisplaySelectedMenuItem = (keyword, category) => {
+    const menuItemsByCategory = (
+        document.querySelectorAll(`.${category}-section .dish`)
+    );
+    menuItemsByCategory.forEach((itemNode) => {
+        if (itemNode.dataset.dish === keyword) {
+            itemNode.classList.add('dish_selected');
+        } else {
+            itemNode.classList.remove('dish_selected');
         }
     });
+};
+
+const changeDisplayBasket = () => {
+    const getBasketItemContent = (item) => {
+        return `${item.name} ${item.price}₽`;
+    };
+
+    let newBasktet = '';
+    let sum = 0;
+    for (let key in selectedMenuItems) {
+        const value = selectedMenuItems[key];
+        const title = textBasketMenuItem[key].title;
+        const content = (
+            selectedMenuItems[key] 
+                ? getBasketItemContent(value) 
+                : textBasketMenuItem[key].default
+        );
+        sum += value ? value.price : 0;
+        newBasktet += createBasketMenuItem(title, content);
+    }
+    newBasktet += createBasketMenuItem('Стоимость заказа', `${sum}₽`);
+
+    document.querySelector('.form__basket-menu-items').innerHTML = newBasktet;
+};
+
+const handleSelectingMenuItem = (e) => {
+    const isDishButton = e.target.classList.contains('dish__add');
+    if (!isDishButton) {
+        return;
+    }
+
+    const keyword = e.target.closest('.dish').dataset.dish;
+    const selectedMenuItem = menu.find(item => item.keyword === keyword);
+    selectedMenuItems[selectedMenuItem.category] = selectedMenuItem;
+
+    changeDisplaySelectedMenuItem(keyword, selectedMenuItem.category);
+    document.getElementById(selectedMenuItem.category).value = keyword;
+    changeDisplayBasket();
+};
+
+const createCard = (card, isHide = false) => {
+    const hideClass = isHide ? 'hide' : '';
+    return `
+    <div class="dish ${hideClass}" data-dish="${card.keyword}">
+        <img class="dish__img" src="${card.image}" alt="${card.name}">
+        <p class="dish__price">${card.price}</p>
+        <p class="dish__name">${card.name}</p>
+        <p class="dish__amount">${card.count}</p>
+        <button class="dish__add">Добавить</button>
+    </div>
+    `;
+};
+
+const displayCards = () => {
+    const menuItemsNodes = {};
+    Object.assign(menuItemsNodes, selectedMenuItems);
+
+    menu
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .forEach((menuItem) => {
+            if (!menuItemsNodes[menuItem.category]) {
+                menuItemsNodes[menuItem.category] = '';
+            }
+            menuItemsNodes[menuItem.category] += createCard(menuItem);
+        });
+
+    for (let key in menuItemsNodes) {
+        const value = menuItemsNodes[key];
+        document.querySelector(`.${key}-section .dishes`).innerHTML = value;
+    }
+};
+
+const changeDisplayCardSection = (kind, category) => {
+    const disheNodes = document.querySelectorAll(`.${category}-section .dish`);
+    disheNodes.forEach((dishNode) => {
+        const keyword = dishNode.dataset.dish;
+        const dishKind = menu.find(item => item.keyword === keyword).kind;
+
+        if ((dishKind === kind) || (kind === 'all')) {
+            dishNode.classList.remove('hide');
+        } else {
+            dishNode.classList.add('hide');
+        }
+    });
+};
+
+const handleSelectingFilter = (e) => {
+    const isFilterItem = e.target.classList.contains('filter__item');
+    if (!isFilterItem) {
+        return;
+    }
+
+    const kind = e.target.dataset.kind;
+    const category = e.target.closest('.section')
+        .classList[1]
+        .replace('-section', '');
+    const isToggleFilter = e.target.classList.contains('filter__item_selected');
+
+    if (isToggleFilter) {
+        e.target.classList.remove('filter__item_selected');
+        changeDisplayCardSection('all', category);
+    } else {
+        const filterItemNodes = (
+            document.querySelectorAll(`.${category}-section .filter__item`)
+        );
+        filterItemNodes.forEach((node) => {
+            node.classList.remove('filter__item_selected');
+        });
+        e.target.classList.add('filter__item_selected');
+        changeDisplayCardSection(kind, category);
+    }
+};
+
+document.querySelector('.main').addEventListener('click', (e) => {
+    handleSelectingMenuItem(e);
+    handleSelectingFilter(e);
+});
+
+displayCards();
